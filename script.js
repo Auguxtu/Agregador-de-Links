0930216fcd993c677e079f079903065d042d3536
“Augusto <augustorosasiq@gmail.com>
2024-09-28 22:50:15 -0300

    againg js?

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function ToggleMode() {
   html.classList.toggle("branco")
 
   const img = document.querySelector("#profile img")
+
   if (html.classList.contains("branco")) {
-    img.setAttribute("src", "/assets/Untitled design.png")
+    img.setAttribute("src", "./assets/careca.png")
   } else {
-    img.setAttribute("src", "/assets/careca.png")
+    img.setAttribute("src", "./assets/Untitled design.png")
   }
 }
